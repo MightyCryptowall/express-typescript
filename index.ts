31c10acab4cef38194b0fa45d82a92842697bf7f
Ye Thiha <mightycryptowall@gmail.com>
2022-04-16 13:40:21 +0630

    prisma -a

--- index.ts
+++ index.ts
@@ -1,12 +1,37 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
+const prisma = new PrismaClient();
+
+app.get('/', async (req: Request, res: Response) => {
+  
+  // await prisma.user.create({
+  //   data: {
+  //     name: 'Alice',
+  //     email: 'alice@prisma.io',
+  //     posts: {
+  //       create: { title: 'Hello World' },
+  //     },
+  //     profile: {
+  //       create: { bio: 'I like turtles' },
+  //     },
+  //   },
+  // })
+  const allUsers = await prisma.user.findMany({
+    include:{
+      posts: true,
+      profile: true
+    }
+  });
+  const posts = await prisma.post.findMany();
+  console.log(posts);
+  console.log(allUsers);
   res.send('Express + TypeScript Server');
 });
 
